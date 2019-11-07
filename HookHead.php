19b8c8d5a35a32ac10babacc05c8b9f90fe132eb
gtbu <30436307+gtbu@users.noreply.github.com>
2019-11-07 13:35:20 +0100

    additional js

--- HookHead.php
+++ HookHead.php
@@ -1,8 +1,9 @@
 <?php
 defined('is_running') or die('Not an entry point...');
 
-global $addonRelativeData, $page;
+global $addonRelativeData, $addonRelativeCode, $page;
 
 $url = common::GetUrl('Special_Blog_Feed');
 $page->head .= '<link rel="alternate" type="application/atom+xml" href="'.$url.'" />';
+$page->head_js[] = $addonRelativeCode . '/static/user.js';
 
